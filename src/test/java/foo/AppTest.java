edf2956449b319b907dc0d7c0f1f1f7ffbc5604d
Kamran p <kamranp@nbos.vinisoft.com>
2016-07-28 16:09:21 +0530

    added new project

---
+++ src/test/java/foo/AppTest.java
@@ -0,0 +1,21 @@
+package foo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class AppTest {
+public  WebDriver driver;
+	
+@Test(description = "send Member profile Information", invocationCount = 1, threadPoolSize = 1, timeOut = 5555555, priority = 1)
+	public void test()
+	{
+		System.out.println("1212121212121");
+		driver = new FirefoxDriver();
+		driver.get("https://google.com");
+		System.out.println("1212155555555555551");
+		
+	}
+	
+}
